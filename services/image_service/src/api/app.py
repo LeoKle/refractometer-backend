57825d77d92858632fc34cc13cc0ee881b47657f
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-03 18:28:23 +0200

    image-service: add dependency container

--- services/image_service/src/api/app.py
+++ services/image_service/src/api/app.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
 
+from containers.container import DependencyContainer
+
 app = FastAPI(title="Refractometer Image Service")
+
+container = DependencyContainer()
+
+container.wire(modules=[])
