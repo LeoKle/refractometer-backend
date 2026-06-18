5c8298c5b5b955fae56bddb9eec0bd8b2adbdc7b
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-18 10:05:20 +0200

    simulation_service: add lightsource parameters model

---
+++ services/simulation_service/src/models/lightsource.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+from models.vector import Vector
+
+
+class LightsourceParameters(BaseModel):
+    direction_vector: Vector
+    support_vector: Vector
+
+    gap_height_meters: float
+    count_rays_height: int
+    gap_width_meters: float
+    count_rays_width: int
+
+    ray_divergence_degrees: float
+    count_diverging_rays: int
