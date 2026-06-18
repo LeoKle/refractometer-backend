f4be0034f3e9524dbcf71940e1eb54f08790fd39
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-18 10:05:07 +0200

    simulation_service: add vector model

---
+++ services/simulation_service/src/models/vector.py
@@ -0,0 +1,35 @@
+import numpy as np
+from pydantic import BaseModel
+
+
+class Vector(BaseModel):
+    x: float
+    y: float
+    z: float
+
+    @classmethod
+    def from_list(cls, values: list[float]):
+        if len(values) != 3:
+            msg = "List must contain exactly three elements"
+            raise ValueError(msg)
+        return cls(x=values[0], y=values[1], z=values[2])
+
+    def to_list(self) -> list[float]:
+        return [self.x, self.y, self.z]
+
+    def to_numpy_array(self) -> np.ndarray:
+        return np.array(self.to_list())
+
+
+class VectorByDistance(BaseModel):
+    base_vector: Vector
+    x_distance: float
+    y_distance: float
+    z_distance: float
+
+
+class VectorByRotation(BaseModel):
+    base_vector: Vector
+    x_rotation_degrees: float
+    y_rotation_degrees: float
+    z_rotation_degrees: float
