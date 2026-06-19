0fdcae486dfe7768f08fef6b69384c36c0c6474d
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-19 10:44:12 +0200

    monolith: add loguru config

--- services/monolith/src/app.py
+++ services/monolith/src/app.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -9,14 +10,19 @@ from controllers.database import (
     simulation_queue_controller,
     simulation_results_controller,
 )
+from log import setup_logging
 from router import root_api_router
 from settings import config
 
+setup_logging()
 container = DependencyContainer()
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("Starting FastAPI app")
     container.init_resources()
     container.wire(
         modules=[
@@ -33,6 +39,7 @@ async def lifespan(app: FastAPI):
     handler.stop()
 
     container.shutdown_resources()
+    logger.info("Shutdown FastAPI app")
 
 
 app = FastAPI(title="Refractometer Backend", version=config.VERSION, lifespan=lifespan)
