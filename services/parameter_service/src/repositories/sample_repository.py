c2f11cf420b67914fcdd251e2af201ca057e379c
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-27 13:03:38 +0200

    parameter_service: update sample repository implementation based on tests

--- services/parameter_service/src/repositories/sample_repository.py
+++ services/parameter_service/src/repositories/sample_repository.py
@@ -1,4 +1,3 @@
-from bson.objectid import ObjectId
 from pymongo.collection import Collection
 
 from interfaces.repositories.sample_repository_interface import ISampleRepository
@@ -8,48 +7,51 @@ from models.sample import Sample
 class SampleRepository(ISampleRepository):
     def __init__(self, collection: Collection):
         self.collection: Collection = collection
+        collection.create_index("id", unique=True)
 
     def find_all(self) -> list[Sample]:
         return [
             Sample(
-                name=sample["name"],
-                id=str(sample["_id"]),
-                sellmeier_coefficients=sample["sellmeierCoefficients"],
+                name=sample.get("name"),
+                id=str(sample.get("id")),
+                sellmeier_coefficients=sample.get("sellmeierCoefficients"),
             )
             for sample in self.collection.find({})
+            if "name" in sample and "sellmeierCoefficients" in sample
         ]
 
     def find_by_id(self, sample_id: str) -> Sample | None:
-        sample_data = self.collection.find_one({"_id": ObjectId(sample_id)})
-        if sample_data:
-            return Sample(
-                name=sample_data["name"],
-                id=str(sample_data["_id"]),
-                sellmeier_coefficients=sample_data["sellmeierCoefficients"],
-            )
-        return None
+        sample_data = self.collection.find_one({"id": sample_id})
+
+        if not sample_data:
+            return None
+
+        return Sample(
+            name=sample_data.get("name"),
+            id=str(sample_data.get("id")),
+            sellmeier_coefficients=sample_data.get("sellmeierCoefficients"),
+        )
 
     def insert(self, sample: Sample):
         self.collection.insert_one({
+            "id": str(sample.id),
             "name": sample.name,
             "sellmeierCoefficients": sample.sellmeier_coefficients.model_dump(),
         })
 
     def update(self, sample: Sample):
-        if not sample.id:
-            msg = "Sample must have an id to be updated"
-            raise ValueError(msg)
-
-        self.collection.update_one(
-            {"_id": ObjectId(sample.id)},
+        result = self.collection.update_one(
+            {"id": str(sample.id)},
             {
                 "$set": {
                     "name": sample.name,
                     "sellmeierCoefficients": sample.sellmeier_coefficients.model_dump(),
                 }
             },
-            upsert=True,
         )
+        if result.matched_count == 0:
+            msg = f"Sample with id {sample.id} not found"
+            raise KeyError(msg)
 
     def delete(self, sample_id: str):
-        self.collection.delete_one({"_id": ObjectId(sample_id)})
+        self.collection.delete_one({"id": sample_id})
