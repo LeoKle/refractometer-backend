72ea259b28b323ba5b677f8e2b2d12decb39267e
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-07-03 15:23:29 +0200

    queue_service: QueueElement set id to UUID as default

--- services/queue_service/src/models/queue_element.py
+++ services/queue_service/src/models/queue_element.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 
 from pydantic import BaseModel, Field
@@ -5,13 +6,13 @@ from pydantic import BaseModel, Field
 
 class QueueElement(BaseModel):
     parameters: dict
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
 
     index: int | None = Field(default=None)
     being_processed: bool = False
 
     issuer: str | None = Field(default=None)
     callback_url: str | None = Field(default=None)
-    id: str | None = Field(default=None)
     name: str | None = Field(default=None)
     image_id: str = Field(default=None)
     issued_at: datetime
