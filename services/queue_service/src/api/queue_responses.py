0b72b856497a0cc6eceff0119ec2ba0ad179d22f
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-07-03 15:06:39 +0200

    queue_service: add SimulationQueueResponse

---
+++ services/queue_service/src/api/queue_responses.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class SimulationQueueResponse(BaseModel):
+    parameters: dict
+
+    index: int | None = Field(default=None)
+    being_processed: bool | None = Field(default=None)
+
+    issuer: str | None = Field(default=None)
+    callback_url: str | None = Field(default=None)
+    id: str | None = Field(default=None)
+    name: str | None = Field(default=None)
+    image_id: str = Field(default=None)
+    issued_at: datetime
+    completed_at: datetime | None = Field(default=None)
